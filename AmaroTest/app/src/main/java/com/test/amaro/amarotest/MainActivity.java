75d5a83240e82dfaf4c6c4ec7ca547a8b119c6ed
Levy <levy.schiavetti@gmail.com>
2018-06-13 07:54:42 -0300

    Setup Options Menu for MainActivity. Add a menu.xml file, icon resources for menu items and icon launcher. Write part of implementation of onOptionsItemSelected

--- AmaroTest/app/src/main/java/com/test/amaro/amarotest/MainActivity.java
+++ AmaroTest/app/src/main/java/com/test/amaro/amarotest/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
                           implements MainContract.View {
@@ -43,5 +45,25 @@ public class MainActivity extends AppCompatActivity
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.filter:
+                break;
+
+            case R.id.sort:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
