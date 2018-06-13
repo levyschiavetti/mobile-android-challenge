e4f281939afdb313164ddeb539117fa5f1f14572
Levy <levy.schiavetti@gmail.com>
2018-06-13 07:48:24 -0300

    In MainActivity, create and write implementation of bindViews(), responsible for binding views from activity_main.xml. For some reason Butterknife is not binding views, will try to solve that later.

--- AmaroTest/app/src/main/java/com/test/amaro/amarotest/MainActivity.java
+++ AmaroTest/app/src/main/java/com/test/amaro/amarotest/MainActivity.java
@@ -2,13 +2,28 @@ package com.test.amaro.amarotest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity
                           implements MainContract.View {
+
+
+    private MainContract.Presenter presenter = new MainPresenter(this);
+    private RecyclerView rv;
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindViews();
+    }
+
+    private void bindViews() {
+        rv = findViewById(R.id.act_main_rv);
+        toolbar = findViewById(R.id.act_main_tb);
     }
 }
