b8ba82319e30f2dccef5aecc35684a8de5a960f0
Levy <levy.schiavetti@gmail.com>
2018-06-13 21:14:27 -0300

    Declare several methods in MainContract.View.

--- AmaroTest/app/src/main/java/com/test/amaro/amarotest/MainContract.java
+++ AmaroTest/app/src/main/java/com/test/amaro/amarotest/MainContract.java
@@ -5,7 +5,12 @@ import java.util.List;
 public interface MainContract {
 
     interface View {
-
+        void updateList(List<ResponseList.Product> list);
+        void toggleLoadingState();
+        void toggleErrorState();
+        void toggleNormalState();
+        void startDetailActivity(ResponseList.Product product, android.view.View ivProduct,
+                                 android.view.View tvName, android.view.View tvPrice);
     }
 
     interface Presenter {
