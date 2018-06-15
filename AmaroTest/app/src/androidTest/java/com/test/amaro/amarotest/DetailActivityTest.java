3565e46130a5dc82a8e88af3c5ab32752381da66
Levy <levy.schiavetti@vadetaxi.com.br>
2018-06-15 14:19:36 -0300

    Create MainActivityTest and DetailActivityTest

---
+++ AmaroTest/app/src/androidTest/java/com/test/amaro/amarotest/DetailActivityTest.java
@@ -0,0 +1,12 @@
+package com.test.amaro.amarotest;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class DetailActivityTest {
+
+
+}
---
+++ AmaroTest/app/src/androidTest/java/com/test/amaro/amarotest/MainActivityTest.java
@@ -0,0 +1,19 @@
+package com.test.amaro.amarotest;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import android.support.test.espresso.*;
+import android.support.test.*;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+
+
+
+
+}
