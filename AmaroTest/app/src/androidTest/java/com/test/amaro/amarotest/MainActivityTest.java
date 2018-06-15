40125e70b17b30a3dfc11099b52d89d289c262ff
Levy <levy.schiavetti@vadetaxi.com.br>
2018-06-15 18:45:35 -0300

    Write basic UI tests in Espresso for MainActivity and DetailActivity.

--- AmaroTest/app/src/androidTest/java/com/test/amaro/amarotest/MainActivityTest.java
+++ AmaroTest/app/src/androidTest/java/com/test/amaro/amarotest/MainActivityTest.java
@@ -1,19 +1,34 @@
 package com.test.amaro.amarotest;
 
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
-import android.support.test.espresso.*;
-import android.support.test.*;
+import android.support.test.rule.ActivityTestRule;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
 
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
+    @Rule
+    public ActivityTestRule<MainActivity> actTestRule = new ActivityTestRule<>(MainActivity.class);
 
-
-
+    @Test
+    public void shouldDisplayToolbar() {
+        onView(withId(R.id.act_main_tb)).check(matches(isDisplayed()));
+        onView(withId(R.id.act_main_fab)).check(matches(isDisplayed()));
+        onView(withId(R.id.act_main_switch_filter)).check(matches(isDisplayed()));
+    }
 
 }
+
+
+
+
