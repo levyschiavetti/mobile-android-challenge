79525dcf918e215f3f2a17919af0d2b63a9dd882
Levy <levy.schiavetti@gmail.com>
2018-06-17 23:40:32 -0300

    DONE!

--- AmaroTest/app/src/main/java/com/test/amaro/amarotest/presentation/DetailActivity.java
+++ AmaroTest/app/src/main/java/com/test/amaro/amarotest/presentation/DetailActivity.java
@@ -3,7 +3,6 @@ package com.test.amaro.amarotest.presentation;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
